52c6908632b39cf4e3c9def529f1e3a92ef30611
t d <tubademir23@gmail.com>
2020-05-20 16:34:32 +0300

    tricks

--- RDDMap/src/main/java/myspark/map/MapFunct.java
+++ RDDMap/src/main/java/myspark/map/MapFunct.java
@@ -9,21 +9,24 @@ import utils.CupModelMap;
 
 public class MapFunct {
 	public static void main(String[] args) {
-		// System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master");
+		// System.setProperty("hadoop.home.dir",
+		// "C:\\hadoop-common-2.2.0-bin-master");
 		CupModelMap map = new CupModelMap("local", "Map Func");
 
-		JavaRDD<CupModel> CupModel_Data = map.mapCupModel("WorldCups.csv", false);
+		JavaRDD<CupModel> CupModel_Data = map.mapCupModel("WorldCups.csv",
+				false);
 
-		JavaRDD<CupModel> Turkey_Data = CupModel_Data.filter(new Function<CupModel, Boolean>() {
-			public Boolean call(CupModel cupModel) throws Exception {
-				return cupModel.hasCup("Turkey");
-			}
-		});
+		JavaRDD<CupModel> Turkey_Data = CupModel_Data
+				.filter(new Function<CupModel, Boolean>() {
+					public Boolean call(CupModel cupModel) throws Exception {
+						return cupModel.hasCup("Turkey");
+					}
+				});
 
 		Turkey_Data.foreach(new VoidFunction<CupModel>() {
 			public void call(CupModel cupModel) throws Exception {
 				System.out.println(cupModel.getYear());
-				cupModel.toString();
+				System.out.println(cupModel.toString());
 			}
 		});
 
--- RDDMap/src/main/java/myspark/map/Union.java
+++ RDDMap/src/main/java/myspark/map/Union.java
@@ -1,5 +1,9 @@
 package myspark.map;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 
@@ -8,34 +12,40 @@ import utils.CupModelMap;
 
 public class Union {
 	public static void main(String[] args) {
-		// System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master");
+		// System.setProperty("hadoop.home.dir",
+		// "C:\\hadoop-common-2.2.0-bin-master");
 		CupModelMap map = new CupModelMap("local", "Union");
 
-		JavaRDD<CupModel> CupModel_Data = map.mapCupModel("WorldCups.csv", false);
+		JavaRDD<CupModel> CupModel_Data = map.mapCupModel("WorldCups.csv",
+				false);
 
-		JavaRDD<CupModel> Turkey_Data = CupModel_Data.filter(new Function<CupModel, Boolean>() {
-			public Boolean call(CupModel cupModel) throws Exception {
-				return cupModel.hasCup("Turkey");
-			}
+		JavaRDD<CupModel> Turkey_Data = CupModel_Data
+				.filter(new Function<CupModel, Boolean>() {
+					public Boolean call(CupModel cupModel) throws Exception {
+						return cupModel.hasCup("Turkey");
+					}
+				});
+		JavaRDD<CupModel> Italy_Data = CupModel_Data
+				.filter(new Function<CupModel, Boolean>() {
+					public Boolean call(CupModel cupModel) throws Exception {
+						return cupModel.hasCup("Italy");
+					}
+				});
+
+		JavaRDD<CupModel> Turkey_Italy_Data = Italy_Data.union(Turkey_Data); //
+		Turkey_Italy_Data.first();
+		System.out.format("T: %s\t I: %s \t Union:  %s", Turkey_Data.count(),
+				Italy_Data.count(), Turkey_Italy_Data.count());
+		try {
+			Files.deleteIfExists(Paths.get("TurkeyAndItaly.csv"));
+			Turkey_Italy_Data.saveAsTextFile("TurkeyAndItaly.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Turkey_Italy_Data.foreach(model -> {
+			System.out.println(model.toString());
 		});
-		JavaRDD<CupModel> Italy_Data = CupModel_Data.filter(new Function<CupModel, Boolean>() {
-			public Boolean call(CupModel cupModel) throws Exception {
-				return cupModel.hasCup("Italy");
-			}
-		});
-
-		JavaRDD<CupModel> Turkey_Italy_Data = Turkey_Data.union(Italy_Data); //
-
-		JavaRDD<String> Turkey_Italy_String_Data = Turkey_Italy_Data.map(new Function<CupModel, String>() {
-			public String call(CupModel cupModel) throws Exception {
-
-				return cupModel.toString();
-			}
-		});
-
-		// sample output
-		Turkey_Italy_String_Data.saveAsTextFile("TurkeyAndItaly.csv");
-
 		map.getContext().close();
 
 	}
